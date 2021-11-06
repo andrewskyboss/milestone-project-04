dbe6ab9e7de87367062bbdd404ba493728ed4cf7
andrewskyboss <andrew.cybossky@gmail.com>
2021-11-06 20:14:05 +0000

    adding product modules and html files

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,39 @@
 from django.contrib import admin
 from .models import Product, Category, Plan
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class PlanAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Plan)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Plan, PlanAdmin)

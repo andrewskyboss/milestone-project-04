3177c0b782284f4ca28f9ae15fc990e00e0c4b01
andrewskyboss <andrew.cybossky@gmail.com>
2021-11-16 00:36:21 +0000

    working on users profile

--- profiles/static/profiles/js/countryfield.js
+++ profiles/static/profiles/js/countryfield.js
@@ -1,11 +1,11 @@
 let countrySelected = $('#id_default_country').val();
 if(!countrySelected) {
-    $('#id_default_country').css('color', '#aab7c4');
+    $('#id_default_country').css('color', '#E0D3DE');
 };
 $('#id_default_country').change(function() {
     countrySelected = $(this).val();
     if(!countrySelected) {
-        $(this).css('color', '#aab7c4');
+        $(this).css('color', '#E0D3DE');
     } else {
         $(this).css('color', '#000');
     }

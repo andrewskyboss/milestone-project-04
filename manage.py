a974ef29a780c98f980af6aba538bacab1da1cd3
andrewskyboss <andrew.cybossky@gmail.com>
2021-11-18 18:29:23 +0000

    testing and style update

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thecube_gym_fitness_club.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'thecube_gym_fitness_club.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

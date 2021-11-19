6be3dc46b35f7089d6ee956c555cfc3771eee95c
andrewskyboss <andrew.cybossky@gmail.com>
2021-11-19 02:13:14 +0000

    adding security key

--- thecube_gym_fitness_club/settings.py
+++ thecube_gym_fitness_club/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d7km9t5jfi8-wighjgdbjj-@*$ztct*gn2)f05_*y+2#rb#2he'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['milestone-project-04-ac.herokuapp.com', 'localhost']
 
